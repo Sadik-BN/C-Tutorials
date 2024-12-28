0528c8abb7b320722dbdfe48c209a7bda3e2e095
Sadik-BN <sadikonline4@gmail.com>
2024-12-29 01:55:01 +0600

    Chapter-1,Problem-1
    Write a program to calculate area of a square

---
+++ Practice problems/chapter-1/problem-1.c
@@ -0,0 +1,12 @@
+//Write a program to calculate area of a square
+# include<stdio.h>
+int main()
+{   
+    printf("\n\n\n   ***Area of A Square***   \n\n");
+    float a;
+    printf("Enter lenth of side of a square (m):- ");
+    scanf("%f",&a);
+    printf("\nThe area is %f meter square.\n\n\n",a*a);
+
+    return 0;
+} 
